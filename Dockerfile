4b7fbfd48c15562db933f0aed3f21006e8b1dcc7
houseleg <kosukefujita979@gmail.com>
2025-01-02 03:07:39 +0900

    FastAPI を追加

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.12.8-slim-bookworm@sha256:10f3aaab98db50cba827d3b33a91f39dc9ec2d02ca9b85cbc5008220d07b17f3
+
+COPY requirements.txt ./
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
